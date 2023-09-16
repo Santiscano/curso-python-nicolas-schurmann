d6f0f27a05962aff55eb5448ad4ac80047d0664d
Santiago Sierra Cano <santiscano@gmail.com>
2023-09-16 11:49:35 -0500

    methods magic

--- 05-clases/08-metodos-magicos.py
+++ 05-clases/08-metodos-magicos.py
@@ -5,9 +5,20 @@ class Perro:
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
-    
+
+    def __del__(self):
+        print(f"chao {self.nombre} siempre te recordare")
+
+    # se llama cuando tratemos de imprimir la clase
     def __str__(self):
-        return f"clase perro: {self.nombre}"
+        return f"Clase Perro: {self.nombre}"
 
     def habla(self):
         print(f"{self.nombre} dice: Woof")
+
+perro1 = Perro('sami', 3)
+perro1.habla()
+# creamos una variable donde se guardara en string la instancia de perro, pero este tambien llamara al metodo __str__
+texto = str(perro1)
+del perro1
+
